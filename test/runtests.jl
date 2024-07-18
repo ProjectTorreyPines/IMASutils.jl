88b9cf49fe0b47d6b65516c7f6cd7f956430b568
Brendan Lyons <lyonsbc@fusion.gat.com>
2024-07-18 15:59:38 -0700

    Update tests and compat, bump version

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using FuseUtils
 using Test
 
 @testset "FuseUtils.jl" begin
-    # Write your tests here.
+    N = 1000
+    x = range(0, 1, N + 1)
+    @test isapprox(trapz(x, x .^ 2), 1.0 / 3.0; rtol=1e-5)
+    @test isapprox(cumtrapz(x, x .^ 2)[(N ÷ 2) + 1], 0.125 / 3.0; rtol=1e-5)
 end
