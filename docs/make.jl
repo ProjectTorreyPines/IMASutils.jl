81e4d65192eeca2ee2e7614d1953d8394d708bd4
Orso Meneghini <orso82@gmail.com>
2024-10-25 12:11:45 -0700

    add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[FuseUtils],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="FuseUtils",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
