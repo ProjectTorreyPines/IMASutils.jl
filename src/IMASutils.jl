8be2a81f0917b056ad6421b9c4d505a3015029c0
Brendan Lyons <lyonsbc@fusion.gat.com>
2024-11-18 20:00:08 -0800

    Create contouring routines

--- src/IMASutils.jl
+++ src/IMASutils.jl
@@ -1,10 +1,15 @@
 module IMASutils
 
+import StaticArrays: SVector, @SVector
+
 include("integration.jl")
 export trapz, cumtrapz
 
 include("optimization.jl")
 export mirror_bound
+
+include("contour.jl")
+export contour_cache, contour_from_midplane, contour_from_midplane!
 
 const document = Dict()
 document[Symbol(@__MODULE__)] = [name for name in Base.names(@__MODULE__, all=false, imported=false) if name != Symbol(@__MODULE__)]
