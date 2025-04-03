393017c4b16f76a659609d5aebbf2de0ee9c430a
Orso Meneghini <orso82@gmail.com>
2025-04-03 15:31:50 -0700

    extend argmin_abs to array - array

--- src/basic.jl
+++ src/basic.jl
@@ -16,8 +16,7 @@ function argmin_abs(x::AbstractVector{<:Real}, x0::Real)
     mini = 0
 
     @inbounds for i in eachindex(x)
-        xi = x[i]
-        d = abs(xi - x0)
+        d = abs(x[i] - x0)
 
         # skip if distance is NaN
         if isnan(d)
@@ -31,7 +30,50 @@ function argmin_abs(x::AbstractVector{<:Real}, x0::Real)
     end
 
     if mini == 0
-        throw(ArgumentError("all elements result in NaN distance from x0"))
+        throw(ArgumentError("all elements result in NaN"))
+    end
+
+    return mini
+end
+
+@inline function argmin_abs(x0::Real, x::AbstractVector{<:Real})
+    return argmin_abs(x, x0)
+end
+
+"""
+    argmin_abs(x::AbstractVector{<:Real}, x0::AbstractVector{<:Real})
+
+Non-allocating version of `argmin(abs.(x .- x0))`.
+
+Returns the index of the element in `x` closest to `x0` in absolute difference.
+
+Throws an error if `x` is empty.
+"""
+function argmin_abs(x::AbstractVector{<:Real}, x0::AbstractVector{<:Real})
+    if isempty(x)
+        throw(ArgumentError("collection must be non-empty"))
+    end
+    @assert length(x) == length(x0)
+
+    minval = NaN
+    mini = 0
+
+    @inbounds for i in eachindex(x)
+        d = abs(x[i] - x0[i])
+
+        # skip if distance is NaN
+        if isnan(d)
+            continue
+        end
+
+        if isnan(minval) || d < minval
+            minval = d
+            mini = i
+        end
+    end
+
+    if mini == 0
+        throw(ArgumentError("all elements result in NaN"))
     end
 
     return mini
